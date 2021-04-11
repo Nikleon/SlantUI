9b88c343a9aff386e7d1a1f48e892b83ebaa7e0f
Wesley Zhuang <zaganon@gmail.com>
2021-04-11 15:57:40 -0700

    made Update implement serializable

--- src/main/java/com/sprutuswesticus/Update.java
+++ src/main/java/com/sprutuswesticus/Update.java
@@ -1,6 +1,7 @@
 package com.sprutuswesticus;
 
-public class Update {
+public class Update implements java.io.Serializable{
+    public int id;
     public int x;
     public int y;
     public int orientation;
