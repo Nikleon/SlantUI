65eb267b65cdce5cca525f515648c19392f20b54
Teraleon <nikleon@gmail.com>
2021-04-12 04:34:42 -0700

    remove id from update

--- src/main/java/com/sprutuswesticus/Update.java
+++ src/main/java/com/sprutuswesticus/Update.java
@@ -5,13 +5,11 @@ import java.io.Serializable;
 public class Update implements Serializable {
     private static final long serialVersionUID = 2585894887518553527L;
 
-    public final int id;
     public final int x;
     public final int y;
     public final int orientation;
 
-    public Update(int id, int x, int y, int orientation) {
-        this.id = id;
+    public Update(int x, int y, int orientation) {
         this.x = x;
         this.y = y;
         this.orientation = orientation;
